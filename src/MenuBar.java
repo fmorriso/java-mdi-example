44f3f73a9c65a6a8939fb40163dbc32c20e4d2b9
Frederick Morrison <frederick.morrison@outlook.com>
2023-03-16 11:32:25 -0600

    add File / Exit submenu item with action to exit the app

--- src/MenuBar.java
+++ src/MenuBar.java
@@ -8,7 +8,32 @@ public class MenuBar extends JMenuBar
         // use frame size passed into us to figure out how big we need to be
         //TODO: *** CODE NEEDED HERE ***
 
-        JMenu file = new JMenu("File");
+        JMenu file = createFileMenu();
         add(file);
+        
+        JMenu edit = createEditMenu();
+        add(edit);
     }
+
+    private JMenu createEditMenu()
+    {
+        JMenu menu = new JMenu("Edit");
+
+        //TODO: *** CODE NEEDED HERE ***
+
+        return menu;
+    }
+
+    private JMenu createFileMenu()
+    {
+        JMenu menu = new JMenu("File");
+        // create sub-menu items:
+        JMenuItem exit = new JMenuItem("Exit");
+        exit.addActionListener(evt -> System.exit(0));
+        menu.add(exit);
+
+        return menu;
+    }
+
+
 }
